53937fc91fca6815fbc86bc91d8a69b7eb494057
doyoonear <doyoonleee@gmail.com>
2022-02-06 01:38:09 +0900

    feature: Add cover image style at blog post page

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -152,6 +152,9 @@ const RenderPost = ({ post, redirect, preview }) => {
         </div>
       )}
       <div className={blogStyles.post}>
+        <div className={blogStyles.coverImg}>
+          <img src={post.pageCover} alt="page-cover-image" />
+        </div>
         <h1>{post.Page || ''}</h1>
         {post.Authors.length > 0 && (
           <div className="authors">By: {post.Authors.join(' ')}</div>
