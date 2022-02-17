c306f137aeda0df4dbdc2381dab638a79768745f
doyoonear <doyoonleee@gmail.com>
2022-02-17 21:29:49 +0900

    refactor: Change header to styled components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Head from 'next/head'
 import ExtLink from './ext-link'
 import { useRouter } from 'next/router'
+import styled, { css } from 'styled-components'
 import styles from '@/styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
@@ -16,7 +17,7 @@ const Header = ({ titlePre = '' }) => {
   const { pathname } = useRouter()
 
   return (
-    <header className={styles.header}>
+    <StyledHeader>
       <Head>
         <title>{titlePre ? `${titlePre} |` : ''} Pear Enough</title>
         <meta
@@ -27,28 +28,70 @@ const Header = ({ titlePre = '' }) => {
         <meta property="og:image" content={ogImageUrl} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.navBar}>
-        <Link href="/">
-          <span className={styles.homeLogo}>🚪</span>
-        </Link>
-        <ul className={styles.navigation}>
+      <NavBar>
+        <HomeButton>
+          <Link href="/">🚪</Link>
+        </HomeButton>
+
+        <Navigation>
           {navItems.map(
             ({ label, page, link }) =>
               page && (
-                <li
+                <NavItem
                   key={page}
                   className={pathname === page ? 'active' : undefined}
                 >
                   <Link href={page}>
                     <a>{label}</a>
                   </Link>
-                </li>
+                </NavItem>
               )
           )}
-        </ul>
-      </div>
-    </header>
+        </Navigation>
+      </NavBar>
+    </StyledHeader>
   )
 }
+
+const StyledHeader = styled.header`
+  display: block;
+  text-align: center;
+  color: white;
+  font-size: 0.93rem;
+  font-weight: 400;
+  letter-spacing: 0.02rem;
+`
+
+const NavBar = styled.div`
+  display: flex;
+  position: fixed;
+  top: 1rem;
+  width: 100%;
+`
+
+const HomeButton = styled.button`
+  font-size: 2rem;
+  width: 6rem;
+  background-color: none;
+`
+
+const Navigation = styled.ul`
+  width: calc(100% - 2rem);
+  list-style: none;
+  padding: 0;
+`
+
+const NavItem = styled.li`
+  display: inline-block;
+  padding: 0 3rem;
+  background-color: var(--black);
+  font-weight: 500;
+  transform: translateY(-0.1rem);
+  margin-left: 3rem;
+
+  .active {
+    background-color: var(--primary);
+  }
+`
 
 export default Header
